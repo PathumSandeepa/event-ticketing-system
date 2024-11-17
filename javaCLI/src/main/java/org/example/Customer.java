6826da4c90588fb5d66dd2477edcbc6e36e9fafa
Pathum Sandeepa <86911717+PathumSandeepa@users.noreply.github.com>
2024-11-17 18:15:38 +0530

    [CLI-04] make vendor and customer classes and add constructor, getters and setters.

---
+++ javaCLI/src/main/java/org/example/Customer.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Customer extends Person {
+
+    public Customer() {
+        super();
+    }
+
+    public Customer(String id, String name) {
+        super(id, name);
+    }
+}
---
+++ javaCLI/src/main/java/org/example/Vendor.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Vendor extends Person {
+
+    public Vendor() {
+        super();
+    }
+
+    public Vendor(String id, String name) {
+        super(id, name);
+    }
+}
